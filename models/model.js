298e6c60c371beffe135fe48f93a4018ccdda6a2
SatvirPunia <punia.satvir.sp@gmail.com>
2023-11-24 12:34:30 -0500

    first commit

---
+++ models/model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import {} from 'dotenv/config'
+
+ export const uri = process.env.MONGO_URI;
+mongoose.connect(uri,{
+    useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(()=>console.log("Connected to Mongodb ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"))
+.catch((err)=>console.log(`Connection Failed due to the error below \n ${err}`))
+
+
+const userSchema = mongoose.Schema({
+
+    name : {type:String,required:true},
+    email : {type:String , required:true},
+    pwd   : {type:String , required:true},
+})
+
+const userModel = mongoose.model("gitHub",userSchema)
+
+export default userModel
+
